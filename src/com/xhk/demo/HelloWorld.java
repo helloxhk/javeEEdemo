c10747155bc3d0b6a3200404f076058cc3cf8dc1
xhk <helloxhk@163.com>
2018-08-18 20:46:21 +0800

    Initial commit

---
+++ src/com/xhk/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.xhk.demo;
+
+/**
+ * 断线风筝︶ㄣ
+ * 2018/8/18 20:36
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World~");
+    }
+}
