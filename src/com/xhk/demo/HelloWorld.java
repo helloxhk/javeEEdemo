4c21ec91a3ca1b2351e8a4c3ccd378c7ba921caa
xhk <helloxhk@163.com>
2018-08-18 22:31:14 +0800

    commit 1

--- src/com/xhk/demo/HelloWorld.java
+++ src/com/xhk/demo/HelloWorld.java
@@ -6,6 +6,6 @@ package com.xhk.demo;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World~");
+        System.out.println("Hello World~ ");
     }
 }
